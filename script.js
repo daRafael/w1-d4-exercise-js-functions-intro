8785c5a47431d3a631b993a1b40c3de78ea67c1c
Rafael Guerra <rafa99gm@hotmail.com>
2024-04-11 12:19:22 +0100

    solved

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 console.log("Ready to go! 🚀");
 
 //const currentYear = new Date().getFullYear();
+
+console.log(userHello('Rafael', '24'));
+
+function userHello(name, age) {
+  return `Hello! ${name}, you are ${age}`;
+}
